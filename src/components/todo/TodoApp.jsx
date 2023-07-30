69551cb9e4e0e3b211f429536c8db815fc3a646d
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2023-07-30 21:45:42 +0200

    Added login component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,7 +1,54 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
 export default function TodoApp(){
     return (
         <div className="TodoApp">
             Todo Managment Application
+            <LoginComponent />
+			{/* <WelcomeComponent /> */}
+        </div>
+    )   
+}
+
+function LoginComponent() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleUsernameChange(event){
+        console.log(event.target.value);
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event){
+        console.log(event.target.value);
+        setPassword(event.target.value);
+    }
+
+    return(        
+        <div className="Login">
+            <div className="LoginForm">
+                <div>
+                    <label>User Name</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
+                </div>
+                <div>
+                    <button type="button" name="login" >Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function WelcomeComponent() {
+    return(
+        <div className="Welcome">
+            Welcome Component
         </div>
     )
 }
