4511b65b30bef7236ca9e59e7750c40cde857d22
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2023-09-21 22:34:43 +0200

    Adding bootstrap to style todo

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,8 +5,8 @@ import './TodoApp.css'
 export default function TodoApp(){
     return (
         <div className="TodoApp">
-            <HeaderComponent/>
             <BrowserRouter>
+            <HeaderComponent/>
                 <Routes>
                     <Route path='/' element={<LoginComponent />}/>
                     <Route path='/login' element={<LoginComponent />}/>
@@ -156,22 +156,35 @@ function ListTodosComponent() {
 
 function HeaderComponent() {
     return(
-        <div className="header">
-            <div>
-                Header <hr />
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+        <div className="container">
+            <div className="row">
+                <nav className="navbar navbar-expand-lg">
+                    <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://react.dev/">React</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/Mariusz">Home</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                        </ul>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
             </div>
         </div>
+    </header>
     )
 }
 
-
 function FooterComponent() {
     return(
-        <div className="footer">
-            <div>
-            <hr />  Footer 
-            </div>
-        </div>
+        <footer className="footer">
+            <div className="container">
+                Welcome 
+            </div>            
+        </footer>
     )
 }
 
