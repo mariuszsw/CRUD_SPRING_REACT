8b14f569680dd3d5a29478eeb12b11b57ad74ae8
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2023-09-23 02:02:46 +0200

    Updating state and verifing updates through authcontext

--- src/components/todo/HeaderComponent.jsx
+++ src/components/todo/HeaderComponent.jsx
@@ -1,11 +1,12 @@
-import {Link} from 'react-router-dom'
-import { AuthContext } from './security/AuthContext'
-import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth } from './security/AuthContext'
 
 function HeaderComponent() {
 
-    const authContext = useContext(AuthContext)
-    console.log(`Footer component = ${authContext.number}`)
+    // const authContext = useContext(AuthContext)
+    const authContext = useAuth();
+
+    console.log(authContext.number)
     return(
         <header className="border-bottom border-light border-5 mb-5 p-2">
         <div className="container">
--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,11 +1,14 @@
-import { createContext } from "react";
-import { useState } from 'react'
+import { createContext, useState, useContext } from "react";
 
 export const AuthContext = createContext();
 
+export const useAuth = () => useContext(AuthContext)
+
 export default function AuthProvider({ children }) {
     const [number, setNumber] = useState(10)
-    
+
+    setInterval(() => setNumber(number + 1),10000)
+
     return (
         <AuthContext.Provider value={{number}}>
             {children}
