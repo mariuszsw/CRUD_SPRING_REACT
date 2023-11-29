a973c6fd3b04ef682aee49550db7febf30958a04
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2023-11-29 11:08:49 +0100

    add functionality to call HelloWorld REST API and handle responses

--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -1,16 +1,35 @@
-import {useParams, Link} from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 
 function WelcomeComponent() {
 
-    const {username} = useParams();
+    const { username } = useParams();
 
-    console.log(username);
+    function callHelloWorldRestApi() {
+        console.log('called')
+        axios.get('http://localhost:8080/hello-world')
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log('cleanup'))
+    }
 
-    return(
+    function successfulResponse(response) {
+        console.log(response)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
+
+    return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
             <div>
                 Manage your todos. <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World REST API</button>
             </div>
         </div>
     )
