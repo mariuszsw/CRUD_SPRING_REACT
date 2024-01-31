09f822011531e0ea1b4338b2e551e7d7030340c8
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-01-31 13:36:52 +0100

    code refactored

--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -1,20 +1,27 @@
 import { useParams, Link } from 'react-router-dom'
-import axios from 'axios'
+import { useState } from 'react';
+import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService';
 
 function WelcomeComponent() {
 
     const { username } = useParams();
 
+    const [message, setMessage] = useState(null)
+
     function callHelloWorldRestApi() {
-        console.log('called')
-        axios.get('http://localhost:8080/hello-world')
+        console.log('called')      
+
+        retrieveHelloWorldPathVariable('Mariusz')
             .then((response) => successfulResponse(response))
             .catch((error) => errorResponse(error))
-            .finally(() => console.log('cleanup'))
+            .finally(() => console.log('cleanup'))    
+
     }
 
     function successfulResponse(response) {
         console.log(response)
+        setMessage(response.data.message)
+        
     }
 
     function errorResponse(error) {
@@ -31,6 +38,7 @@ function WelcomeComponent() {
             <div>
                 <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World REST API</button>
             </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
