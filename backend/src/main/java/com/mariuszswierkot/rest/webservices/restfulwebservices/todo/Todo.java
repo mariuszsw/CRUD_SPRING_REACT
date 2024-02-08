34e473754c4d9ccaf70b6728e763a5c7b9da77da
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-02-08 18:08:15 +0100

    Add TodoResource class with retrieveTodos method and added some fix

--- backend/src/main/java/com/mariuszswierkot/rest/webservices/restfulwebservices/todo/Todo.java
+++ backend/src/main/java/com/mariuszswierkot/rest/webservices/restfulwebservices/todo/Todo.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 public class Todo {
 
 	public Todo() {
-		
+
 	}
-	
+
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
 		this.id = id;
@@ -20,7 +20,7 @@ public class Todo {
 	private int id;
 
 	private String username;
-	
+
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;
@@ -70,5 +70,4 @@ public class Todo {
 		return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", targetDate="
 				+ targetDate + ", done=" + done + "]";
 	}
-
 }
