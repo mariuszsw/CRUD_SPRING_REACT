054f8a25c65c8234f8ab075724f94bf68c1655e4
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-02-22 11:30:12 +0100

    creating retrive todo and delete todo

--- backend/src/main/java/com/mariuszswierkot/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ backend/src/main/java/com/mariuszswierkot/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -2,6 +2,8 @@ package com.mariuszswierkot.rest.webservices.restfulwebservices.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,15 @@ public class TodoResource {
 	public List<Todo> retieveTodos(@PathVariable String username) {
 		return todoService.findByUsername(username);
 	}
-
+	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo retieveTodo(@PathVariable String username, @PathVariable int id) {
+		return todoService.findById(id);
+	}		
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+		 todoService.deleteById(id);
+		 return ResponseEntity.noContent().build();
+	}	
 }
