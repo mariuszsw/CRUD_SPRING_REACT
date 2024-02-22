9d398146933234199cba174ac3e5981f8a2e03d8
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2024-02-22 12:28:47 +0100

    adding delete feature to frontend

--- frontend/src/components/todo/ListTodosComponent.jsx
+++ frontend/src/components/todo/ListTodosComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+import { deleteTodoApi, retrieveAllTodosForUsernameApi } from "./api/TodoApiService";
 
 function ListTodosComponent() {
 
@@ -9,29 +9,46 @@ function ListTodosComponent() {
 
     const [todos, setTodos] = useState([])
 
+    const [message, setMessage] = useState(null)
+
+
     useEffect(
         () => refreshTodos(), []
     )
 
     function refreshTodos() {
-        retrieveAllTodosForUsername('Mariusz')
+        retrieveAllTodosForUsernameApi('Mariusz')
             .then(response => {
                 // console.log(response.data)
                 setTodos(response.data)
             })
             .catch(error => console.log(error))
     }
+
+    function deleteTodo(id) {
+        console.log("Clicked" + id)
+        deleteTodoApi('Mariusz', id)
+            .then(
+                () => {
+                    setMessage(`Delete of todo with id = ${id} successful`)
+                    refreshTodos()
+                }
+            )
+            .catch(error => console.log(error))
+    }
+
     return (
         <div className="container">
             <h1>Things You Want to Do!</h1>
+            {message && <div className="alert alert-warning">{message}</div>}
             <div>
                 <table className='table'>
                     <thead>
                         <tr>
-                            <td>ID</td>
-                            <td>description</td>
-                            <td>id Done?</td>
-                            <td>Target Date</td>
+                            <th>description</th>
+                            <th>id Done?</th>
+                            <th>Target Date</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,10 +56,10 @@ function ListTodosComponent() {
                             todos.map(
                                 todo => (
                                     <tr key={todo.id}>
-                                        <td>{todo.id}</td>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
                                         <td>{todo.targetDate.toString()}</td>
+                                        <td> <button className="btn btn-warning" onClick={() => deleteTodo(todo.id)}>Delete</button></td>
                                     </tr>
                                 )
                             )
